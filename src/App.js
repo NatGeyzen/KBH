bd91ce9a0bdb3ecf091c5457da8f5a76954c98ed
Nat Geyzen <nat.geyzen@outlook.com>
2020-04-25 17:28:40 -0500

    new header component mock-up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './App.css';
 
+import Header from './components/fixed-components/Header/Header';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/fixed-components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = props => {
+    return (
+        <div className="Header">
+            <div className="LogoContainer">
+                logo
+            </div>
+            <div className="MockUpTitle">HEADER</div>
+            <div className="ButtonContainer">
+                toggle button
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
