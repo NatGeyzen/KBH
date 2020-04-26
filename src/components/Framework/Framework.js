4c1b0ea77bf30d23485626556db431f6cdef08b5
Nat Geyzen <nat.geyzen@outlook.com>
2020-04-26 17:18:26 -0500

    new folder restucture (framework components now inside framework folder)

--- src/components/Framework/Framework.js
+++ src/components/Framework/Framework.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 
-import Header from '../Header/Header';
-import Navigation from '../Navigation/Navigation';
-import Footer from '../Footer/Footer';
+import Header from './Header/Header';
+import Navigation from './Navigation/Navigation';
+import Footer from './Footer/Footer';
 
 const Framework = () => {
     return (
